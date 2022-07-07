78a4afa0e3c5a23a9485237331713d546d8a05f3
yangjinrui325 <2430960358@qq.com>
2022-07-07 18:01:28 +0800

    [style]样式优化

--- src/layout/SideBar.vue
+++ src/layout/SideBar.vue
@@ -1,14 +1,34 @@
 <template>
   <div class="bar-wrapper">
     <t-card :bordered="false" shadow class="web-card">
-      <div class="user-head" flex="main:center">
-        <t-avatar size="100px" class="mr-auto"> W </t-avatar>
+      <div class="user-head" flex="dir:top main:center cross:center">
+        <t-avatar size="100px" class="user-head__photo"> W </t-avatar>
+        <h1 class="text-base cursor">漪洛</h1>
+        <p class="text-extra">漪洛的个人小破站</p>
+        <p class="user-head__icon">
+          <AppIcon />
+          <BacktopRectangleIcon />
+          <BarcodeIcon />
+        </p>
       </div>
     </t-card>
   </div>
 </template>
 <script setup lang="ts">
+import {
+  AppIcon,
+  BacktopRectangleIcon,
+  BarcodeIcon,
+} from "tdesign-icons-vue-next";
+
 const infoMessage = "这就是";
 </script>
 <style lang="less" scoped>
+.user-head {
+  padding: 10px 0;
+  text-align: center;
+  &__icon {
+    font-size: 18px;
+  }
+}
 </style>
